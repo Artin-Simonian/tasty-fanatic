2d47f1e721d8771cba8ee6cdcb79aa82ddfb9fa8
Artin-Simonian <artinsimonian@yahoo.com>
2023-09-13 13:18:41 -0700

    comments

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -8,21 +8,15 @@ module.exports = {
   };
 
   async function editReview(req, res) {
-    // Note the cool "dot" syntax to query on the property of a subdoc
     const recipe = await Recipe.findOne({'reviews._id': req.params.id});
-    // Find the comment subdoc using the id method on Mongoose arrays
-    // https://mongoosejs.com/docs/subdocs.html
     const review = recipe.reviews.id(req.params.id);
-    // Render the comments/edit.ejs template, passing to it the comment
     res.render('comments/edit', { review });
   }
 
   async function deleteReview(req, res) {
-    // Note the cool "dot" syntax to query on the property of a subdoc
     const recipe = await Recipe.findOne({ 'reviews._id': req.params.id, 'reviews.user': req.user._id });
-    // Rogue user!
     if (!recipe) return res.redirect('/recipes');
-    // Remove the review using the remove method available on Mongoose arrays
+
     recipe.reviews.remove(req.params.id);
     // Save the updated movie doc
     await recipe.save();
